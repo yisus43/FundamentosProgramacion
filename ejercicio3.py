41f36c7042d3da76ee12ec70272f5b50ddf1e35f
Jesus <uzumaki95lobo@gmail.com>
2024-10-11 13:53:54 -0600

    ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,21 @@
+'''
+Ejercicio 3:
+    Programa que calcula la hipotenusa a partir de dos catetos
+Entradas:
+    catetoA:int -> a
+    catetoB:int ->b
+Salidas
+    hipotenusa:float ->
+Analisis:
+    Se utiliza el teorema de pitagoras
+'''
+import math
+
+
+a=input("Escribe el valor del cateto A:")
+a=int(a)
+b=input("Escribe el valor del cateto B:")
+b=int(b)
+c=(a*a+b*b)**(1/2)
+c=math.sqrt(a**2+b**2)
+print("El valor de la hipotenusa es:",c)
