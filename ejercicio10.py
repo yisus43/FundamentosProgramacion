292877434108279eb773f51a76f727690e7577ff
Jesus <uzumaki95lobo@gmail.com>
2024-10-17 13:46:17 -0600

    ejercicio 9

---
+++ ejercicio10.py
@@ -0,0 +1,14 @@
+n1=input("Nota del parcial 1:")
+n1=int(n1)
+n2=input("Nota del parcial 2:")
+n2=int(n2)
+n3=input("Nota del parcial 3:")
+n3=int(n3)
+ne=input("Nota del examen:")
+ne=int(ne)
+nt=input("Nota del trabajo:")
+nt=int(nt)
+c=(n1+n2+n3)/3*0.55+0.3*ne+0.15*nt
+print=("Nota Final:",c)
+
+
---
+++ ejercicio9.py
@@ -0,0 +1,12 @@
+'''
+Una tienda ofrece un descuento del 15% sibre el total de la compra y un cliente desea saebr cuanto debera pagar 
+finalmente por su compra
+
+Entradas:
+    pre:interger
+Salidas:
+    precio final
+'''
+pre=input("Escribe el precio de la compra:")
+pre=int(pre)
+print("El precio final es:",pre-pre*0-15)
